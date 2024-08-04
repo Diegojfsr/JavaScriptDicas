0cea4e0d603839cd89f04b27f576f42e99247939
DiegoJfsr <diegojfsr@gmail.com>
2024-08-03 23:00:51 -0300

    DataStructured

---
+++ DataStructured/ArraysHoraDeCodar/Arrays.js
@@ -0,0 +1,16 @@
+
+var arr = [5, "Diego", true, {n1:1, n2:2}]
+console.log(arr);
+
+var arr2 = ["javascript", "java", "python"];
+console.log(arr2);
+
+//exibe o valor pelo indice
+console.log(arr[1]);
+console.log(arr2[0]);
+
+arr[3] = 10; //adiciona o elemento 10 no indice 4
+arr2[0] = "c++"; // adiciona o lemento c++ no indeice 0 porem substitui o que tava no lugar
+
+console.log(arr);
+console.log(arr2);
